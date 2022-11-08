a508aa695b74904c0a006686796a563b80fe65ca
Aseempaul007 <aseempal007@gmail.com>
2022-11-08 12:09:47 +0530

    Second commit!

--- app/src/main/java/com/example/widgetsapp/MainActivity.java
+++ app/src/main/java/com/example/widgetsapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.widgetsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox cb1;
+    CheckBox cb2;
+    CheckBox cb3;
+    RadioButton rb1;
+    RadioButton rb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cb1 = findViewById(R.id.checkBox);
+        cb2 = findViewById(R.id.checkBox2);
+        cb3 = findViewById(R.id.checkBox3);
+        rb1 = findViewById(R.id.radioButton);
+        rb2 = findViewById(R.id.radioButton2);
+
+
+
     }
 }
